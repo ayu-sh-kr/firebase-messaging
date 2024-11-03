2d2c84b34700706a307c7f09073cc7117a9b161c
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-03 23:26:54 +0530

    Initial Commit

---
+++ src/main/java/dev/archimedes/firebasemessaging/FirebaseMessagingApplication.java
@@ -0,0 +1,13 @@
+package dev.archimedes.firebasemessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseMessagingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FirebaseMessagingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/archimedes/firebasemessaging/FirebaseMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.archimedes.firebasemessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseMessagingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
