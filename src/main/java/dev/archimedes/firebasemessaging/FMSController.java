316ba1f9cb942c07a252d319f3f677c6ec6becc0
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-04 15:27:34 +0530

    Added controller to expose API for sending request from backend server

---
+++ src/main/java/dev/archimedes/firebasemessaging/FMSController.java
@@ -0,0 +1,44 @@
+package dev.archimedes.firebasemessaging;
+
+import com.google.firebase.messaging.FirebaseMessagingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/fms")
+@RequiredArgsConstructor
+public class FMSController {
+
+
+  private final MessageUsingTokenUseCase messageUsingTokenUseCase;
+  private final MessageUsingTopicUseCase messageUsingTopicUseCase;
+
+  @PostMapping("/v1/singleton")
+  @ResponseStatus(HttpStatus.OK)
+  void send(SingleMessageRequest request) throws FirebaseMessagingException {
+    messageUsingTokenUseCase.execute(request);
+  }
+
+  @PostMapping("/v1/broadcast")
+  @ResponseStatus(HttpStatus.OK)
+  void send(BatchMessageRequest request) throws FirebaseMessagingException {
+    messageUsingTokenUseCase.execute(request);
+  }
+
+  @PostMapping("/v1/topic")
+  @ResponseStatus(HttpStatus.OK)
+  void send(TopicMessageRequest request) throws FirebaseMessagingException {
+    messageUsingTopicUseCase.execute(request);
+  }
+
+
+  @ResponseStatus(HttpStatus.CONFLICT)
+  @ExceptionHandler(FirebaseMessagingException.class)
+  void handler(FirebaseMessagingException exception) {
+    log.info(exception.getMessage());
+  }
+
+}
